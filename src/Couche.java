a95e2c2d2de06fe976b53832cbb6c3ab8dbd6643
hugoSip <hugo.sipahi@hotmail.fr>
2016-03-13 00:57:08 +0100

    couche + neurone update

--- src/Couche.java
+++ src/Couche.java
@@ -3,4 +3,13 @@ public class Couche {
 
 	private Neurone[] neurones;
 	private int nbNeurones;
+	
+	public Couche(int nbNeu, int nbEntrees){
+		nbNeurones = nbNeu;
+		neurones = new Neurone[nbNeurones];
+		for(int i = 0; i<nbNeurones; i++){
+			Neurone neurone = new Neurone (nbEntrees);
+			neurones[i]=neurone;
+		}
+	}
 }
