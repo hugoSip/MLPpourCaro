be53bdd3c736a8b8802a0f889c6fdc45dadf7392
hugoSip <hugo.sipahi@hotmail.fr>
2016-03-12 23:54:53 +0100

    "debut neurones + couches"

--- src/Couche.java
+++ src/Couche.java
@@ -1,4 +1,5 @@
 
 public class Couche {
 
+	private Neurone[] neurones;
 }
--- src/Neurone.java
+++ src/Neurone.java
@@ -1,4 +1,13 @@
 
 public class Neurone {
-
+	private int nbEntree;
+	private int nbSortie;
+	private int[] poids;
+	private int[] entrees;
+	private int[] sortie;
+	
+	public void Neurone(int nbE, int nbS){
+		nbEntree = nbE;
+		nbSortie = nbS;
+	}
 }
