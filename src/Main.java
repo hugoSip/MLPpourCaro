2fbf814a3e2dc4a0f3a839b164af0fef66c22cf9
Florian Wahl <florian.wahl@hotmail.fr>
2016-03-13 00:38:57 +0100

    Pretraitement v0.1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,15 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Pretraitement pr = new Pretraitement("data/glass.data");
+		double[][] data = pr.getDataTable();
+		
+		for(int i = 0; i < data.length; i++){
+			for(int j = 0; j < data[0].length; j++){
+				System.out.print(data[i][j] + " | ");
+			}
+			System.out.println("");
+		}
 	}
 
 }
